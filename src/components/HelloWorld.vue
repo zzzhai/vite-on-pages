cdb626c5ddbc4dfc8e023da8f894ea15f7f3cb5f
zzzhai <zzzhai@icloud.com>
2023-04-13 18:39:25 +0800

    修改了主页文字及标题

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,8 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
+      想快速拥有一个自己控制的网站么？<br>
+      Let's go！<br>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
